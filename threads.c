d8ebc4dc053ce00b84632ee7fffc319f112432b1
Victor <vicfaraujo@gmail.com>
2020-11-12 21:03:51 -0300

    Ok no Linux

--- threads.c
+++ threads.c
@@ -7,7 +7,7 @@
 
 //int main(int nArgs,char* arquivo[]){
 int main(){
-    char c, buffer[50];
+    char buffer[20];
 	uint32_t matrixNumber, rowsA, columnsA, rowsB, columnsB, matrixCounter;
 	float **matrixA, **matrixB, *resultMatrix, sum;
 
@@ -18,11 +18,11 @@ int main(){
 		printf("Vazio\n");
 		exit(0);
 	}
-	fscanf(file,"%s",&buffer);
+	fscanf(file,"%s",buffer);
 	matrixNumber = atoi(buffer);
-	// fscanf(file,"%s",&buffer);
+	// fscanf(file,"%s",buffer);
 	// rows = atoi(buffer);
-	// fscanf(file,"%s",&buffer);
+	// fscanf(file,"%s",buffer);
 	// columns = atoi(buffer);
 	// printf("MatrixssNumbers: %d\nRows:%d\nColumns:%d\n",matrixNumber,rows,columns);
     // matrixA = malloc(matrixNumber * sizeof(float));
@@ -30,15 +30,15 @@ int main(){
     
     
 	while(!feof(file)) {
-		fscanf(file,"%s",&buffer);
+		fscanf(file,"%s",buffer);
 		rowsA = atoi(buffer);
-		fscanf(file,"%s",&buffer);
+		fscanf(file,"%s",buffer);
 		columnsA = atoi(buffer);
 		matrixA = malloc(rowsA * sizeof(float));
 		for (int i=0; i < rowsA; i++) {
 			matrixA[i] = malloc(columnsA * sizeof(float));
 			for (int j=0; j < columnsA; j++) {
-        		fscanf(file,"%s",&buffer);
+        		fscanf(file,"%s",buffer);
                 // printf("%s ",buffer);
 				matrixA[i][j] = atof(buffer);
                 if (matrixA[i][j] >= 0) printf(" ");
@@ -48,16 +48,16 @@ int main(){
 		}
 		printf("\n");
 
-		fscanf(file,"%s",&buffer);
+		fscanf(file,"%s",buffer);
 		rowsB = atoi(buffer);
-		fscanf(file,"%s",&buffer);
+		fscanf(file,"%s",buffer);
 		columnsB = atoi(buffer);
 		// matrixB = malloc(rowsB * columnsB * sizeof(float));
 		matrixB = malloc(rowsB * sizeof(float));
 		for (int i=0; i < rowsB; i++) {
 			matrixB[i] = malloc(columnsB * sizeof(float));
 			for (int j=0; j < columnsB; j++) {
-        		fscanf(file,"%s",&buffer);
+        		fscanf(file,"%s",buffer);
                 // printf("%s ",buffer);
 				matrixB[i][j] = atof(buffer);
                 if (matrixB[i][j] >= 0) printf(" ");
@@ -67,7 +67,7 @@ int main(){
 		}
 		printf("\n");
 		resultMatrix = malloc(rowsA*columnsB*sizeof(float));
-		printf("multiplicacao\n");
+		printf("multiplicacaooo\n");
 		for (int i=0; i < rowsA; i++) {
 			// sum = 0;
 			for (int j=0; j < columnsB; j++) {
@@ -98,8 +98,10 @@ int main(){
         // //printf("%s\n",buffer);
         
         
-		// free(matrixA);
 	}
+	free(matrixA);
+	free(matrixB);
+	free(resultMatrix);
     // printf("%f",matrixA[3][(3*columns)+2]);
     // free(matrixA);
 
