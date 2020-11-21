5cc02de4ca52bb60a073290c02ca7de3b053f263
Victor <vicfaraujo@gmail.com>
2020-11-21 19:25:34 -0300

    Saída ok, tempo não.

--- backup-funcionandoantesthreads.c
+++ backup-funcionandoantesthreads.c
@@ -8,11 +8,6 @@
 #define N 4
 
 
-struct parameters{
-    uint32_t counter;
-    FILE* output;
-};
-
 // Implementação com as matrizes em uma linha
 
 uint32_t matrixNumber, rowsA, columnsA, rowsB, columnsB, matrixCounter=0;
@@ -22,12 +17,11 @@ float **matrixA, **matrixB, **resultMatrix;
 int step_i = 0;
 
 // void matrixProduct(double *resultMatrix, float **matrixA, float **matrixB, uint32_t rowsA, uint32_t rowsB, uint32_t columnsA, uint32_t columnsB){
-void matrixProduct(uint32_t matrixCounter){
+void matrixProduct(FILE* output, uint32_t matrixCounter){
 	float sum = 0;
     int core = step_i++;
-    printf("%d\n",step_i);
-	// for (uint32_t i=0; i < rowsA; i++) {
-    for (int i= core * rowsA/4; i < (core+1)*rowsA/4; i++) {
+	for (uint32_t i=0; i < rowsA; i++) {
+    // for (int i= core * rowsA/4; i < (core+1)*rowsA/4; i++) {
         // sum = 0;
         // printf(" ");
         for (uint32_t j=0; j < columnsB; j++) {
@@ -62,18 +56,17 @@ void matrixProduct(uint32_t matrixCounter){
             // if (abs(resultMatrix[(i*columnsB)+j]) < 100) printf(" ");
             // printf("%0.2f ",resultMatrix[(i*columnsB)+j]);
 
-            // if (resultMatrix[matrixCounter][(i*columnsB)+j] >= 0) printf(" ");
-            // if (abs(resultMatrix[matrixCounter][(i*columnsB)+j]) < 10) printf(" ");
-            // if (abs(resultMatrix[matrixCounter][(i*columnsB)+j]) < 100) printf(" ");
-            // printf("%0.2f ",resultMatrix[matrixCounter][(i*columnsB)+j]);
-            // if(i == rowsA -1 && j == columnsB - 1) printf("%0.2f ",resultMatrix[matrixCounter][(i*columnsB)+j]);
+            if (resultMatrix[matrixCounter][(i*columnsB)+j] >= 0) printf(" ");
+            if (abs(resultMatrix[matrixCounter][(i*columnsB)+j]) < 10) printf(" ");
+            if (abs(resultMatrix[matrixCounter][(i*columnsB)+j]) < 100) printf(" ");
+            printf("%0.2f ",resultMatrix[matrixCounter][(i*columnsB)+j]);
 
             // if (sum >= 0) printf(" ");
             // printf("%0.2f ",sum);
 
 
         }
-        printf("\n");
+        // printf("\n");
     }
 }
 
@@ -108,7 +101,6 @@ int main(){
     resultMatrix = malloc(matrixNumber * sizeof(float*));
     
 	while(!feof(file)) {
-        printf("STEP ATUAL: %d",step_i);
 		fscanf(file,"%s",buffer);
 		rowsA = atoi(buffer);
 		fscanf(file,"%s",buffer);
@@ -119,7 +111,17 @@ int main(){
 
         matrixA[matrixCounter] = malloc(rowsA*columnsA*sizeof(float));
 
-
+		// for (int i=0; i < rowsA; i++) {
+		// 	matrixA[i] = malloc(columnsA* sizeof(float));
+		// 	for (int j=0; j < columnsA; j++) {
+        // 		fscanf(file,"%s",buffer);
+        //         // printf("%s ",buffer);
+		// 		matrixA[i][j] = atof(buffer);
+        //         // if (matrixA[i][j] >= 0) printf(" ");
+		// 		// printf("%0.2f ",matrixA[i][j]);
+		// 	}
+		// 	// printf("\n");
+		// }
 
         for (int i=0; i < rowsA; i++) {
 			for (int j=0; j < columnsA; j++) {
@@ -141,6 +143,20 @@ int main(){
         // resultMatrix= malloc(rowsA * columnsB * sizeof(float));
 
 
+		// matrixB = malloc(rowsB * columnsB * sizeof(float));
+		// matrixB = malloc(rowsB * sizeof(float*));
+		// for (int i=0; i < rowsB; i++) {
+		// 	matrixB[i] = malloc(columnsB * sizeof(float));
+		// 	for (int j=0; j < columnsB; j++) {
+        // 		fscanf(file,"%s",buffer);
+        //         // printf("%s ",buffer);
+		// 		matrixB[i][j] = atof(buffer);
+        //         // if (matrixB[i][j] >= 0) printf(" ");
+		// 		// printf("%0.2f ",matrixB[i][j]);
+		// 	}
+		// 	// printf("\n");
+		// }
+
         for (int i=0; i < rowsB; i++) {
 			for (int j=0; j < columnsB; j++) {
         		fscanf(file,"%s",buffer);
@@ -151,24 +167,7 @@ int main(){
 		
         // printf("M%d\n",matrixCounter);
 		// if(matrixCounter == 0) matrixProduct(resultMatrix,matrixA,matrixB,rowsA,rowsB,columnsA,columnsB);
-		// matrixProduct(matrixCounter);
-
-
-
-        for (uint32_t i = 0; i < N; i++) { 
-            int* p; 
-            pthread_create(&threads[i], NULL, matrixProduct, matrixCounter); 
-        } 
-    
-        // joining and waiting for all threads to complete 
-        for (int i = 0; i < N; i++)  
-            pthread_join(threads[i], NULL);
-        // int *p;
-        // pthread_exit(p);
-        step_i = 0;  
-
-
-
+		matrixProduct(output, matrixCounter);
 
 		matrixCounter++;
 
@@ -182,41 +181,33 @@ int main(){
         
         
 	}
-    // for (uint32_t contador=0; contador < matrixNumber; contador++) {
-    //     matrixProduct(contador);
-    //     printf("\n");
-    // }
-    // matrixProduct(output,0);
 
-	// for (int j=0; j < matrixNumber ; j++){
-    //     for (int i = 0; i < N; i++) { 
-    //         int* p; 
-    //         pthread_create(&threads[i], NULL, matrixProduct, j); 
+    // for (uint32_t i = 0; i < 2; i++) {
+    //     printf("M%d\n",i);
+    //     for (uint32_t j = 0; j < rowsA; j++) {
+    //         // printf(" ");
+    //         for (uint32_t k=0; k < columnsB; k++) {
+    //             if (resultMatrix[i][(j*columnsB)+k] >= 0) printf(" ");
+    //             if (abs(resultMatrix[i][(j*columnsB)+k]) < 10) printf(" ");
+    //             if (abs(resultMatrix[i][(j*columnsB)+k]) < 100) printf(" ");
+    //             printf("%0.2f ",resultMatrix[i][(j*columnsB)+k]);
+    //             // if(j == rowsA -1 && k == columnsB - 1) printf("%0.2f ",resultMatrix[i][(j*columnsB)+k]);
+    //         }
+    //         // printf("\n");
     //     }
-    //     for (int i = 0; i < N; i++)  
-    //         pthread_join(threads[i], NULL);  
-    // }
-
-    for (uint32_t i = 0; i < 2; i++) {
-        fprintf(output,"M%d\n",i);
-        for (uint32_t j = 0; j < rowsA; j++) {
-            // fprintf(output," ");
-            for (uint32_t k=0; k < columnsB; k++) {
-                if (resultMatrix[i][(j*columnsB)+k] >= 0) fprintf(output," ");
-                if (abs(resultMatrix[i][(j*columnsB)+k]) < 10) fprintf(output," ");
-                if (abs(resultMatrix[i][(j*columnsB)+k]) < 100) fprintf(output," ");
-                fprintf(output,"%0.2f",resultMatrix[i][(j*columnsB)+k]);
-                // if(j == rowsA -1 && k == columnsB - 1) fprintf(output,"%0.2f ",resultMatrix[i][(j*columnsB)+k]);
-            }
-            fprintf(output,"\n");
-        }
-        fprintf(output,"\n");      
+    //     printf("\n");      
         
-    }
+    // }
     
 
+	// for (int i = 0; i < N; i++) { 
+    //     int* p; 
+    //     pthread_create(&threads[i], NULL, matrixProduct, output); 
+    // } 
   
-    // joining and waiting for all threads to complete 
+    // // joining and waiting for all threads to complete 
+    // for (int i = 0; i < N; i++)  
+    //     pthread_join(threads[i], NULL);  
 
 
     // for (int i = 0 ; i < rowsA; i++){
@@ -234,6 +225,8 @@ int main(){
 	free(matrixA);
 	free(matrixB);
 	free(resultMatrix);
+    // printf("%f",matrixA[3][(3*columns)+2]);
+
 	fclose(file);
 	return 0;
 }
